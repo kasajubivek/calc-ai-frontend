4f27bc7a4e5e00bd19e3641557ec3c4305771824
Bivek Kasaju <bkasaju97@gmail.com>
2025-01-26 23:51:19 -0500

    tserrorsfixes

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
+// @ts-ignore
+import eslint from 'vite-plugin-eslint';
 import path from "path"
 import react from "@vitejs/plugin-react"
-import eslint from 'vite-plugin-eslint';
 import { defineConfig } from "vite"
  
 export default defineConfig({
